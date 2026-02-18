528506d052032687d59fab635281d065ad78e153
William Mansfield <william.mansfield@mymail.champlain.edu>
2026-02-17 20:15:13 -0500

    added some comments and got a little bit of bouncing reflections in

--- animal3D SDK/resource/glsl/4x/fs/00-common/drawRT_fs4x.glsl
+++ animal3D SDK/resource/glsl/4x/fs/00-common/drawRT_fs4x.glsl
@@ -82,7 +82,7 @@ float rand(vec2 co){
     return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
 }
 
-vec3 CreateRayDirection(vec3 target, vec3 start)
+vec3 CreateRayDirection(vec3 target, vec3 start) // take two positions and return a unit vector 
 {
 	return normalize(target - start);
 }
@@ -90,7 +90,7 @@ vec3 CreateRayDirection(vec3 target, vec3 start)
 vec3 CreateRandomRayDirectionOnHem()//this will get a new randon direction on a unit sphere 
 {
 
-//get a random vector
+	//get a random vector
 	float x = rand(vTexcoord_atlas.xy);
 	float y = rand(vTexcoord_atlas.xy);
 	float z = rand(vTexcoord_atlas.yz);
@@ -101,8 +101,7 @@ vec3 CreateRandomRayDirectionOnHem()//this will get a new randon direction on a 
 	randVec = normalize(randVec);
 
 	//dot against the normalize
-
-	vec4 normal = vTangentBasis_view[3];
+	vec4 normal =  normalize(vTangentBasis_view[2]);
 	vec3 testNormal = vec3(normal.x, normal.y, normal.z);
 
 	if(dot(randVec, testNormal) > 0.0)
@@ -114,14 +113,14 @@ vec3 CreateRandomRayDirectionOnHem()//this will get a new randon direction on a 
 }
 
 //TODO might need to split this up from a test if hit (bool) and have get color as seperate options)
-bool RayCastSphere(const vec3 rayPos, vec3 objPos, float r)
+bool RayCastSphere(const vec3 rayStartPos, vec3 rayDirection, vec3 objPos, float r)
 {
 	vec4 color;
 
 	//use geo position
-	vec3 rayTarget = vTangentBasis_view[3].xyz;
+	//vec3 rayTarget = vTangentBasis_view[3].xyz;
 
-	//ray target in screen space coridnates  -->not used
+	//ray target in screen space coridnates  -->not used but if you can figure out what uAxis is be my guest
 	float depth = gl_FragCoord.z;
 	vec4 uAxis = vec4(0.0); //viewer_stack[0].viewProjectionBiasMatInverse; //error --> this might need to be its own uniform?
 	vec4 imagePos = vec4(gl_FragCoord.xy * uAxis.zw, depth, 1.0); //uAxsis might be the uniform Axis meaning the orientatino of the camera
@@ -131,11 +130,20 @@ bool RayCastSphere(const vec3 rayPos, vec3 objPos, float r)
 
 
 	//ray direction
-	vec3 rayDir = CreateRayDirection(rayTarget, rayPos);
+	vec3 rayDir = rayDirection;
 
-	
+	//This section is a attempt at not hitting collision that happen beind the surface normal
+//	vec4 normal =  normalize(vTangentBasis_view[2]); 
+//
+//	//not facing the center
+//	if(dot(rayDir, normal.xyz) > 0)
+//	{
+//		//false;
+//		return false;
+//	}
+
 	//SPHERE TEST
-	vec3 s = objPos - rayPos;
+	vec3 s = objPos - rayStartPos;
 	float b = dot(rayDir, s);
 	float c = dot(s, s) - r * r;
 
@@ -146,15 +154,20 @@ bool RayCastSphere(const vec3 rayPos, vec3 objPos, float r)
 		return false;
 	}
 
-	//TODO: we can check if the ray is hitting from the back here so that we can get some fake lighting
+
 	d = b - sqrt(d);
 
-	vec3 hitPos = rayPos + d * rayDir; //get a position on the thing to hit
+//	if(d < 0.0)
+//	{
+//		return false;
+//	}
+
+	vec3 hitPos = rayStartPos + d * rayDir; //get a position on the thing to hit
 	posView = vec4(hitPos, 1.0);
 	posBias = viewer_stack[0].projectionBiasMat * posView;
 	//gl_FragDepth = posBias.z / posBias.w; //somthing is messed up with the depth
 
-	vec3 nrlHit = normalize(hitPos - objPos);
+	vec3 nrlHit = normalize(hitPos - objPos); //if you need the hit normal
 
 	return true;
 
@@ -163,6 +176,7 @@ bool RayCastSphere(const vec3 rayPos, vec3 objPos, float r)
 //this function gets the color of a object at a model index
 vec3 GetColorOfObject(int modelIndex)
 {
+//get the color based off of the object that we hit
 	vec3 color = vec3(0.0, 0.0, 0.0);
 
 	switch(modelIndex)
@@ -190,38 +204,48 @@ void main()
 	//TODO: make a ray from the camera in a direction
 		//camera origin * look direction
 
-	const vec3 rayPos = vec3(0.0); //TODO: change this to the camera at some point
+	const vec3 rayPos = vec3(0,0,0); //starting ray.. in this case the center of the scene
+									//TODO: change this to the position of the camera or some point light
 
+	//bootstrap case
+	vec3 objPos = model_stack[IDX_MODEL_SPHERE1].modelViewMat[3].xyz; //set the first object to be tested
+	vec3 rayDirection =  CreateRayDirection(vTangentBasis_view[3].xyz, rayPos); //make a ray that will always hit
 	for(int i = 0; i < 3; i++) //first pass check for hits
 	{
+		rayHit = false;
 		//hit first sphere
-		vec3 objPos = model_stack[IDX_MODEL_SPHERE1].modelViewMat[3].xyz;
-		if(RayCastSphere(rayPos, objPos, radius_sphere1)) //check if we hit the object
+		if(RayCastSphere(rayPos, rayDirection, objPos, radius_sphere1) && !rayHit) //check if we hit the object
 		{
-			hitIndexs[i] = IDX_MODEL_SPHERE1;
-			//TODO change the starting and target position of the vector so we can have a new ray to test against
+			rayHit = true;
+			hitIndexs[i] = IDX_MODEL_SPHERE1; //add it to the hit list
 
-		
+			//change the starting direction by a unit vector on a hemisphere
+			rayDirection = CreateRandomRayDirectionOnHem();
 		}
 
 		//hit second sphere
 		vec3 nextPos = model_stack[IDX_MODEL_SPHERE0].modelViewMat[3].xyz; 
-		if(RayCastSphere(rayPos, nextPos, radius_sphere0))
+		if (RayCastSphere(rayPos, rayDirection, nextPos, radius_sphere0) && !rayHit)
 		{
-			hitIndexs[i] = IDX_MODEL_SPHERE0;
+			rayHit = true;
+			hitIndexs[i] = IDX_MODEL_SPHERE0;//add it to the hit list for color
+			rayDirection = CreateRandomRayDirectionOnHem();//calcualte new direction
 		}
+
+		//TODO add a box check here aswell as the last sphere
 
 	}
 
 	vec3 totalColor;
 	for(int i = 2; i > -1; i--) //make second pass for coloring
 	{
-	//you can add some influece based off of the bounce depth
-		totalColor += GetColorOfObject(hitIndexs[i]);
+		//this parese the hit list and colors back ward from the last hit object
+		//you can add some influece based off of the bounce depth
+		totalColor += (GetColorOfObject(hitIndexs[i]));
 	}
 	
 
-	rtFragColor.rgb = totalColor;
+	rtFragColor.rgb = totalColor; //out put total color
 	rtFragColor.a = 1.0;
 
 
