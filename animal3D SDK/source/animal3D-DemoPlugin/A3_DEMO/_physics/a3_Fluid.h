01f31ea07e7426b77344ee2bc40e9747c55dc023
William Mansfield <william.mansfield@mymail.champlain.edu>
2026-03-18 12:38:41 -0400

    added files for the fluid sim code

---
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_physics/a3_Fluid.h
@@ -0,0 +1,21 @@
+
+
+#ifndef __ANIMAL3D_A3DX_FLUID_H
+#define __ANIMAL3D_A3DX_FLUID_H
+
+
+#include "animal3D-A3DM/animal3D-A3DM.h"
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif	// __cplusplus
+
+
+#ifdef __cplusplus
+}
+#endif	// __cplusplus
+
+
+#endif // #ifndef __ANIMAL3D_A3DX_HULL_H
