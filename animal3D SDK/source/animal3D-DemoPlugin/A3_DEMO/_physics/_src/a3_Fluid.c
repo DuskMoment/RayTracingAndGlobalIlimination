45b7dfe5b9c1f80f320b0fecc63c02892627e544
William Mansfield <william.mansfield@mymail.champlain.edu>
2026-03-18 14:09:32 -0400

    finished on way of inital set up for the thing

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_physics/_src/a3_Fluid.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_physics/_src/a3_Fluid.c
@@ -7,3 +7,132 @@
 
 
 //-----------------------------------------------------------------------------
+
+a3ret InitFluidGrid(a3_FluidGrid* grid, a3i32 gridSize, a3i32 gridCountX, a3i32 gridCountY, a3i32 gridCountZ)
+{
+	
+	grid = malloc(sizeof(a3_FluidGrid));
+
+
+	if (grid != NULL)
+	{
+		grid->gridCountX = gridCountX;
+		grid->gridCountY = gridCountY;
+		grid->gridCountZ = gridCountZ;
+
+		grid->gridSize = gridSize;
+		
+		//good data
+		return  1;
+	}
+
+	return -1;
+}
+
+//TODO CHANGE THIS TO THE RIGHT CONSTANT FOR SAFTY -- this method is also extreamly slow...
+a3ret InitVelocity(a3_FluidGrid* fluidGrid)
+{
+	//failed
+	if (fluidGrid == NULL)
+	{
+		return -1;
+	}
+
+	a3i32 x = fluidGrid->gridCountX;
+	a3i32 y = fluidGrid->gridCountY;
+	a3i32 z = fluidGrid->gridCountZ;
+
+
+	//ALLOCATE THE X  + 1 GIRD for final
+	a3real*** array = (a3real***)malloc(x + 1 * sizeof(a3real**));
+
+	for (int i = 0; i < x + 1; i++) {
+
+		array[i] = (a3real**)malloc(y * sizeof(a3real*));
+
+		for (int j = 0; j < y; j++) {
+
+			array[i][j] = (a3real*)malloc(z * sizeof(a3real3));
+		}
+
+	}
+	fluidGrid->velocityXFinal = array;
+
+	//ALLOCATE FOR THE X + 1 GRIND for temp
+	array = (a3real***)malloc(x + 1 * sizeof(a3real**));
+
+	for (int i = 0; i < x + 1; i++) {
+
+		array[i] = (a3real**)malloc(y * sizeof(a3real*));
+
+		for (int j = 0; j < y; j++) {
+
+			array[i][j] = (a3real*)malloc(z * sizeof(a3real3));
+		}
+
+	}
+	fluidGrid->velocityXTemp = array;
+
+	//ALLOCATE FOR THE Y + 1 GRIND for final
+	array = (a3real***)malloc(x * sizeof(a3real**));
+
+	for (int i = 0; i < x; i++) {
+
+		array[i] = (a3real**)malloc(y + 1 * sizeof(a3real*));
+
+		for (int j = 0; j < y + 1; j++) {
+
+			array[i][j] = (a3real*)malloc(z * sizeof(a3real3));
+		}
+
+	}
+	fluidGrid->velocityYFinal = array;
+
+	//ALLOCATE FOR THE Y + 1 GRIND for temp
+	array = (a3real***)malloc(x  * sizeof(a3real**));
+
+	for (int i = 0; i < x; i++) {
+
+		array[i] = (a3real**)malloc(y + 1 * sizeof(a3real*));
+
+		for (int j = 0; j < y + 1; j++) {
+
+			array[i][j] = (a3real*)malloc(z * sizeof(a3real3));
+		}
+
+	}
+	fluidGrid->velocityYTemp = array;
+
+
+	//ALLOCATE FOR THE Z + 1 GRIND for final
+	array = (a3real***)malloc(x * sizeof(a3real**));
+
+	for (int i = 0; i < x; i++) {
+
+		array[i] = (a3real**)malloc(y  * sizeof(a3real*));
+
+		for (int j = 0; j < y; j++) {
+
+			array[i][j] = (a3real*)malloc(z + 1 * sizeof(a3real3));
+		}
+
+	}
+	fluidGrid->velocityZFinal = array;
+
+	//ALLOCATE FOR THE Z + 1 GRIND for temp
+	array = (a3real***)malloc(x * sizeof(a3real**));
+
+	for (int i = 0; i < x; i++) {
+
+		array[i] = (a3real**)malloc(y * sizeof(a3real*));
+
+		for (int j = 0; j < y; j++) {
+
+			array[i][j] = (a3real*)malloc(z + 1 * sizeof(a3real3));
+		}
+
+	}
+	fluidGrid->velocityZTemp = array;
+	
+	return -1;
+}
--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_physics/a3_Fluid.h
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/_physics/a3_Fluid.h
@@ -5,13 +5,16 @@
 
 
 #include "animal3D-A3DM/animal3D-A3DM.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif	// __cplusplus
-	typedef struct FluidGrid
+	typedef struct a3_FluidGrid
 	{
 		//the ammout of cells for each portion of the gird x,y,z
 		a3i32 gridCountX;
@@ -26,14 +29,25 @@ extern "C"
 		//array of veclotiys final
 		a3real*** velocityXFinal;
 		a3real*** velocityYFinal;
-		a3real*** velocityYFinal;
+		a3real*** velocityZFinal;
 
 		//array of velocitys temp 
 		a3real*** velocityXTemp;
 		a3real*** velocityYTemp;
 		a3real*** velocityZTemp;
 
-	};
+	}a3_FluidGrid;
+
+	//----------------------
+
+	//init the grid
+	a3ret InitFluidGrid(a3_FluidGrid* grid, a3i32 gridSize, a3i32 gridCountX, a3i32 gridCountY, a3i32 gridCountZ);
+
+	//init the velocitys
+	a3ret InitVelocity(a3_FluidGrid* fluidGrid);
+
+	a3ret DestroyFluidGrid(a3_FluidGrid* grid);
+	
 #ifdef __cplusplus
 }
 #endif	// __cplusplus
